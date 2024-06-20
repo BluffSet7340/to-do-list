dcc21037506c255e00985b6484c5207880a9e874
Saeed <sbm7640@outlook.com>
2024-06-20 16:41:17 +0400

    added buttons, and text for testing

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 
 function ToDoList() {
   // a useState hook, initialized to an empty array
-  const [task, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    "Eat Breakfast",
+    "Shower",
+    "Walk the dog",
+  ]);
   const [newTask, setNewTask] = useState("");
 
   //when user types into the textbox
-  function handleInputChange(event) {}
+  function handleInputChange(event) {
+    //setNewTask is the function that updates the state of newTask
+    // we access the event paramter, its target and then its value
+    setNewTask(event.target.value);
+  }
 
   function addTask() {}
 
@@ -22,27 +30,82 @@ function ToDoList() {
   function editTask() {}
 
   return (
-    <div classNameName="to-do-list">
+    <div className="to-do-list">
       <h1>To-Do List</h1>
-      <div>
+      <div  className="d-flex justify-content-center">
         {/* for taking in user input, reduced width of search bar only so that the
         search bar and button appear side by side */}
-        <form className="d-flex" role="search" >
           <input
-            className="form-control"
+            className=""
             type="text"
             placeholder="Enter a Task..."
             aria-label="Search"
             value={newTask}
-            style={{width:"200px"}}
+            style={{ width: "200px" }}
             // When the input is changed, we call the Javascript function
             onChange={handleInputChange}
           />
-          <button className="btn btn-outline-success" type="submit">
-            Enter Task
+          {/* when button is clicked, we have a callback to the function addTask */}
+          <button
+            className="btn btn-outline-success add-button"
+            type="submit"
+            onClick={addTask}
+          >
+            Add Task
           </button>
-        </form>
       </div>
+      <ol>
+        {/* in tasks, each "task" is mapped to an <li> tag. We refer to each task as the variable "task" */}
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button
+              type="button"
+              className="btn btn-danger delete-button"
+              // with regards to a callback, when passed in an argument, the function is executed right away.
+              //To avoid this, we use an arrow function
+              onClick={() => {
+                deleteTask(index);
+              }}
+            >
+              Delete
+            </button>
+            <button
+              type="button"
+              className="btn btn-info move-button"
+              // with regards to a callback, when passed in an argument, the function is executed right away.
+              //To avoid this, we use an arrow function
+              onClick={() => {
+                moveTaskUp(index);
+              }}
+            >
+              Up
+            </button>
+            <button
+              type="button"
+              className="btn btn-info move-button"
+              // with regards to a callback, when passed in an argument, the function is executed right away.
+              //To avoid this, we use an arrow function
+              onClick={() => {
+                moveTaskDownk(index);
+              }}
+            >
+              Down
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-warning"
+              // with regards to a callback, when passed in an argument, the function is executed right away.
+              //To avoid this, we use an arrow function
+              onClick={() => {
+                moveTaskDownk(index);
+              }}
+            >
+              Edit
+            </button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
